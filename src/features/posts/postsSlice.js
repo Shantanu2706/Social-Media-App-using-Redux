3eb12edec689f7f1db2cceb8240a6a3a271e056e
shantanu <shantanu.pimprikar@bajajfinserv.in>
2023-10-19 00:14:28 +0530

    adding new posts

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -7,6 +7,17 @@ const initialState = {
     error: null
 }
 
+export const addNewPost = createAsyncThunk(
+    'posts/addNewPost',
+    // The Payload creator receives the partial `{title,content,user}` object
+    async initialPost => {
+        // We send the initial data to the fake API server
+        const response = await client.post('/fakeApi/posts', initialPost)
+        // The response includes the complete post object, including unique OD
+        return response.data
+
+    })
+
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     const response = await client.get('/fakeApi/posts')
     return response.data
@@ -61,6 +72,10 @@ const postsSlice = createSlice({
             .addCase(fetchPosts.rejected, (state, action) => {
                 state.status = 'failed'
                 state.error = action.error.message
+            }),
+            builder.addCase(addNewPost.fulfilled, (state, action) => {
+                // We can directly add the new post object to our posts array
+                state.posts.push(action.payload)
             })
     }
 })
